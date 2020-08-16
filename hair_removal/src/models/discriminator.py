738d0f9550d5beba8b44eb738d61e5736f045002
Hyunju <hyunjulee@image.cau.ac.kr>
2020-08-17 05:35:16 +0900

    Update discriminator.py

--- hair_removal/src/models/discriminator.py
+++ hair_removal/src/models/discriminator.py
@@ -2,8 +2,9 @@ import torch
 import torch.nn as nn
 
 class CNNDiscriminator(nn.Module):
-    def __init__(self):
+    def __init__(self, input_height, input_width):
         super(CNNDiscriminator, self).__init__()
+        self.last_output_size = int((input_height / (2**5)) * (input_width / (2**5)))
         self.model = nn.Sequential(
             nn.Conv2d(3, 64, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
@@ -27,7 +28,7 @@ class CNNDiscriminator(nn.Module):
         )
 
         self.linear = nn.Sequential(
-            nn.Linear(1024*4*4, 1),
+            nn.Linear(1024*self.last_output_size, 1),
         )
 
     def forward(self, img):
